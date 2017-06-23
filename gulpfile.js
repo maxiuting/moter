2295415c0b95a4a62b21b0a31aaabb639ec7bc40
maxiuting <2528079160@qq.com>
2017-06-23 21:08:36 +0800

    就

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp=require('gulp');
+var minifyHtml=require('gulp-minify-html');
+var minifyCss=require('gulp-clean-css');
+
+gulp.task('htmltask',function(){
+	gulp.src('*.html')
+	.pipe(minifyHtml())
+	.pipe(gulp.dest('dist'))
+})
+
+gulp.task('csstask',function(){
+	gulp.src('./css/*.css')
+	.pipe(minifyCss())
+	.pipe(gulp.dest('./dist/css'))
+})
